be2c9334773db824973d0f69ea18e61e8fc343fb
edenojieh-png <edenojieh@gmail.com>
2025-08-24 13:30:04 -0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~dncnhJza#mHO3f4qSczlEMqYw1xGxncNLIbS83yXtEr1kRR9Nf6E",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
